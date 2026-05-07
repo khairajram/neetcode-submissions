846e11327b1f70413e7ee0050f3857a3e6365770
Khairaj Ram <145283068+khairajram@users.noreply.github.com>
2026-05-07 17:52:13 +0530

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& t) {
+        vector<int> st = {0,0,0};
+
+        for(auto i : triplets){
+            if(i[0] > t[0] || i[1] > t[1] || i[2] > t[2])
+                continue;
+            
+            st[0] = max(st[0],i[0]);
+            st[2] = max(st[2],i[2]);
+            st[1] = max(st[1],i[1]);
+        }
+
+        return st == t;
+    }
+};
