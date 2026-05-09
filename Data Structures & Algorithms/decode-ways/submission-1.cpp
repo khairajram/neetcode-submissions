5e01b7e5032e32df8da21091d9bf8202e196302e
Khairaj Ram <145283068+khairajram@users.noreply.github.com>
2026-05-09 17:55:48 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int solve(string s,int i,vector<int> &dp){
+        if(i == s.size())
+            return 1;
+        else if(s[i] == 0)
+            return 0;
+        else if(dp[i] != -1)
+            return dp[i];
+
+        int ways = solve(s,i+1,dp);
+
+        if(i + 1 < s.size()){
+            int num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+
+            if(num >= 10 && num <= 26)
+                ways += solve(s, i + 2, dp);
+        }
+
+        return dp[i] = ways;
+    }
+public:
+    int numDecodings(string s) {
+        if(s[0] == '0')
+            return 0;
+        int n = s.size();
+        vector<int> dp(n,-1);
+        return solve(s,0,dp);
+    }
+};
